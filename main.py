db2260f6bf821927aa6ca2ec2975d146e619dd50
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-05-19 12:21:29 +0200

    Refactor script

--- main.py
+++ main.py
@@ -3,29 +3,31 @@ __winc_id__ = '7b9401ad7f544be2a23321292dd61cb6'
 __human_name__ = 'arguments'
 
 # Add your code after this line
+
+
 # Part 1: Greet Template
 def greet(name, greeting_template='Hello, <name>!'):
     return greeting_template.replace('<name>', name)
 
+
 # Part 2: Force
 def force(mass, body='earth'):
-    celestial_bodies = [
-        {'name': 'sun', 'gravity': 274},
-        {'name': 'jupiter', 'gravity': 24.9},
-        {'name': 'neptune', 'gravity': 11.2},
-        {'name': 'saturn', 'gravity': 10.4},
-        {'name': 'earth', 'gravity': 9.8},
-        {'name': 'uranus', 'gravity': 8.9},
-        {'name': 'venus', 'gravity': 8.9},
-        {'name': 'mars', 'gravity': 3.7},
-        {'name': 'mercury', 'gravity': 3.7},
-        {'name': 'moon', 'gravity': 1.6},
-        {'name': 'pluto', 'gravity': 0.6},
-    ]
-    for celestial_body in celestial_bodies:
-        if body == celestial_body['name']:
-            return mass * celestial_body['gravity']
+    bodies = {
+        'sun': 274,
+        'jupiter': 24.9,
+        'neptune': 11.2,
+        'saturn': 10.4,
+        'earth': 9.8,
+        'uranus': 8.9,
+        'venus': 8.9,
+        'mars': 3.7,
+        'mercury': 3.7,
+        'moon': 1.6,
+        'pluto': 0.6,
+    }
+    return mass * bodies[body]
+
 
 # Part 3: Gravity
 def pull(m1, m2, d):
-    return (6.674 * 10 ** -11) * ((m1 * m2) / (d**2))
+    return (6.674 * 10 ** -11) * ((m1 * m2) / d**2)
